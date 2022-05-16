940cf7b11021c237c0275300676297673ff6606f
Luke Evangelista <cpbm_3sh@yahoo.com>
2022-05-16 21:21:09 +0800

    .gitignore is now working

--- EmployeeSQL/employees_tableschema.sql
+++ EmployeeSQL/employees_tableschema.sql
@@ -81,5 +81,6 @@ REFERENCES "employees" ("emp_no");
 select * from departments;
 select * from dept_emp;
 select * from dept_manager;
+select * from employees;
 select * from salaries;
 select * from titles;
